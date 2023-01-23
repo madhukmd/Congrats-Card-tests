d886001fc0767ddd4b98a94165d3b3f2d010d04d
madhukmd <kondamadhu1994@gmail.com>
2023-01-23 20:15:31 +0000

    Congrats Card tests

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="bgContainer">
+    <h1 className="heading main">Congratulations</h1>
+    <div className="cardContainer">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="heading">User 1</h1>
+      <p className="Paragraph">
+        User.One Institute of Computer Education and technology Hyderabad.
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
